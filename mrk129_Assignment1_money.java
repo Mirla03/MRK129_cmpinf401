ec5f439a3456835f52e5579b8b8b9950f58bd889
Mirla03 <112401952+Mirla03@users.noreply.github.com>
2022-09-12 22:34:32 -0400

    Update mrk129_Assignment1_money.java

--- mrk129_Assignment1_money.java
+++ mrk129_Assignment1_money.java
@@ -1,5 +1,26 @@
 package test;
 
+import javax.swing.JOptionPane;
+import static java.lang.Math.PI;
+
 public class mrk129_Assignment1_money {
 
-}
+	public static void main(String[] args)  {
+	
+	String m = JOptionPane.showInputDialog("Enter a number between 1-9999");
+	int dollars = Integer.parseInt(m);
+	
+	int grand = dollars/1000;
+	int remain1 = dollars % 1000;
+	
+	int Benjamins = remain1/100;
+	int remain2 = remain1 % 100;
+	
+	int Sawbucks = remain2/10;
+	int remain3 = remain2 % 10;
+	
+	int Bucks = remain3;
+	
+	JOptionPane.showMessageDialog(null,grand + " Grands, " + Benjamins + " Benjamins, " + Sawbucks + " sawbucks, "+ Bucks + " bucks");
+	
+	}}

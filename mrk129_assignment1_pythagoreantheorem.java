5d00322006cd229634c9312000a775687c2e60a0
Mirla03 <112401952+Mirla03@users.noreply.github.com>
2022-09-12 22:35:05 -0400

    Update mrk129_assignment1_pythagoreantheorem.java

--- mrk129_assignment1_pythagoreantheorem.java
+++ mrk129_assignment1_pythagoreantheorem.java
@@ -1,13 +1,21 @@
 package test;
 
 import javax.swing.JOptionPane;
+import static java.lang.Math.sqrt;
 
 public class mrk129_assignment1_pythagoreantheorem {
-	
+
 	public static void main(String[] args) {
-		
-		String input = JOptionPane.showInputDialog("Write your name");
-		JOption.PaneshowMessageDialog(null,"Hi"+input);
+
+		String input1 = JOptionPane.showInputDialog("Enter a number");
+		String input2 = JOptionPane.showInputDialog("Enter a number");
+		int num1 = Integer.parseInt(input1);
+		int num2 = Integer.parseInt(input2);
+		double hypotenuse = Math.sqrt(Math.pow(num1, 2)+Math.pow(num2, 2));
+		hypotenuse = hypotenuse * 100;
+		int r = (int) Math.round(hypotenuse);
+		hypotenuse = r/100;
+		JOptionPane.showMessageDialog(null,"The hypotenuse is " + hypotenuse);
 
 
 	}
